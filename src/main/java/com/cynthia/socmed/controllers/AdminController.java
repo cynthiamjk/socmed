479054409fcd130ca446f9ef587c14c4ed2c1e54
cynthiamjk <cynthia1777@hotmail.fr>
2020-07-27 21:37:01 +0200

    more 'report post'stuff

--- src/main/java/com/cynthia/socmed/controllers/AdminController.java
+++ src/main/java/com/cynthia/socmed/controllers/AdminController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -28,8 +29,28 @@ public class AdminController {
         }
 
         List<Report> reports = (List<Report>) reportDao.findAll();
-        int n = reports.size();
-        modelMap.addAttribute("number", n);
+        List<Report> postReports = new ArrayList<>();
+        List<Report> userReports = new ArrayList<>();
+        List<Report> eventReports = new ArrayList<>();
+
+        for(Report report: reports) {
+            if (report.getEvent() != null) {
+                eventReports.add(report);
+            }
+            if (report.getPost() != null) {
+                postReports.add(report);
+            }
+            if (report.getUser() != null) {
+                userReports.add(report);
+            }
+        }
+
+        int postNumber = postReports.size();
+        int eventNumber = eventReports.size();
+        int userNumber = userReports.size();
+        modelMap.addAttribute("postNumber", postNumber);
+        modelMap.addAttribute("eventNumber", eventNumber);
+        modelMap.addAttribute("userNumber", userNumber);
         return "adminPage";
     }
 
